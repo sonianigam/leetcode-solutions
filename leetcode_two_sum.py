2f318db4e9e7a37a8933c3f942a30ef268ff93ca
sonianigam <code@getbraintree.com>
2019-04-26 11:50:33 -0700

    Use dictionary to solve two sum problem

---
+++ leetcode_two_sum.py
@@ -0,0 +1,15 @@
+import numpy
+
+class Solution:
+    def two_sum(self, nums: list, target: int) -> list:
+        nums_dict = dict()
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in nums_dict.keys():
+                print(nums_dict)
+                return [min(i, nums_dict[complement]), max(i, nums_dict[complement])]
+            else:
+                nums_dict[nums[i]] = i
+            
---
+++ leetcode_two_sum_test.py
@@ -0,0 +1,6 @@
+import pytest
+import leetcode_two_sum
+
+def test_two_sum():
+    assert leetcode_two_sum.Solution().two_sum([2, 6, 7, 15], 9) == [0,2]
+
