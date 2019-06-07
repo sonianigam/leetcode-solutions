2ee2943b02b0a593f25d0433486dacbf7d21715f
sonianigam <sonianigam2017@u.northwestern.edu>
2019-06-07 11:18:33 -0700

    Update logic to account for overflow

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,13 +1,14 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        num_list = [digit for digit in str(x)]
+        num = str(x)
 
-        if num_list[0] == "-":
-            neg = num_list.pop(0)
-            num_list.append(neg)
+        if num[0] == "-":
+            num = num[1:] + "-"
 
-        num_list.reverse()
-        r_num = int("".join(num_list))
+        r_num  = int(num[::-1])
 
         if r_num <= -2**31 or r_num >= 2**31 - 1: return 0
         return r_num
+
+        print(num)
+
