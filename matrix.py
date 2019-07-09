81e7b0db43032031961c8f57d8130cb15b47ae2e
sonianigam <sonianigam2017@u.northwestern.edu>
2019-07-09 13:58:49 -0700

    First iteration of matrix problem

---
+++ matrix.py
@@ -0,0 +1,45 @@
+from typing import List
+import numpy as np
+import pdb
+
+class Solution:
+    def update_matrix(self, matrix:List[List[int]]) -> List[List[int]]:
+        num_rows = len(matrix)
+        num_columns = len(matrix[0])
+        zero_indices = []
+        #replace
+        output = [[1,1,1],
+                 [1,1,1],
+                 [1,1,1]]
+
+        for x in range(num_rows):
+            for y in range(num_columns):
+                if matrix[x][y] == 0:
+                    output[x][y] = 0
+                    zero_indices.append((x,y))
+        
+        for x in range(num_rows):
+            for y in range(num_columns):
+                min_dist = 10000
+                if output[x][y] == 0:
+                    break
+
+                for z in zero_indices:
+                    if min_dist == 1:
+                        break
+
+                    x_dist = abs(z[0] - x)
+                    y_dist = abs(z[1] - y)
+                    total_dist = x_dist + y_dist
+                    
+                    if total_dist < min_dist:
+                        min_dist = total_dist
+
+                output[x][y] = min_dist
+        return output
+
+### Questions
+# float(inf)?
+# if statement in one line
+# set up list without linking
+# formatted print
---
+++ matrix_test.py
@@ -0,0 +1,24 @@
+import pytest
+import matrix
+
+def test_case_one():
+    m_input = [[0,0,0],
+               [0,1,0],
+               [0,0,0]]
+
+    m_output = [[0,0,0], 
+                [0,1,0], 
+                [0,0,0]]
+
+    assert(matrix.Solution().update_matrix(m_input) == m_output)
+
+def test_case_two():
+    m_input = [[0,0,0],
+               [0,1,0],
+               [1,1,1]]
+
+    m_output = [[0,0,0], 
+                [0,1,0], 
+                [1,2,1]]
+
+    assert(matrix.Solution().update_matrix(m_input) == m_output)
